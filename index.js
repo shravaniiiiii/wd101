4e8c097f288a5eea1883edd07fc0b1acb7d48b74
shravaniiiiii <151656004+shravaniiiiii@users.noreply.github.com>
2023-11-21 23:32:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,73 @@
-// Event listener for DOMContentLoaded event
-document.addEventListener('DOMContentLoaded', () => {
-  // Get the reference to the user table body
-  const userTableBody = document.getElementById('userTableBody');
-
-  // Loop through all localStorage items
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-
-    // Check if the key starts with 'user_'
-    if (key.startsWith('user_')) {
-      // Parse the JSON-stored user data from localStorage
-      const userData = JSON.parse(localStorage.getItem(key));
-
-      // Add the user data to the table
-      addRowToTable(userTableBody, userData);
+let userForm = document.getElementById("user-form");
+ let userEntries=[]
+const retrieveEntries=()=>{
+    let entries=localStorage.getItem("user-entries");
+    if(entries){
+        entries=JSON.parse(entries);
+    }else{
+        entries=[]
     }
-  }
-});
+    return entries;
+};
+const displayEntries=()=>{
+    const entries=retrieveEntries();
+    const tableEntries = entries.map((entry)=>{
+        const namecell = `<td>${entry.name}</td>`;
+        const emailcell = `<td>${entry.email}</td>`;
+        const passwordcell = `<td>${entry.password}</td>`;
+        const dobcell = `<td>${entry.dob}</td>`;
+        const acceptTermscell = `<td>${entry.acceptedTermsAndconditions}</td>`;
+        const row=`<tr>${namecell} ${emailcell} ${passwordcell} ${dobcell} ${acceptTermscell}</tr>`;
+        return row
+    }).join('\n');
+    const table=`<thead><tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Password</th>
+    <th>Dob</th>
+    <th>Accepted Terms?</th>
+    </tr></thead>
+    <body>${tableEntries}</body>`;
+    let details=document.getElementById("user-entries");
+    details.innerHTML=table;
+};
 
-// Event listener for form submission
-const form = document.getElementById('registrationForm');
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent default form submission
 
-  // Get the user's date of birth from the form
-  const dob = new Date(form.dob.value);
-
-  // Calculate the user's age based on the current year
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - dob.getFullYear();
-
-  // Validate the user's age to ensure it's between 18 and 55
-  if (!(age >= 18 && age <= 55)) {
-    alert('Age should be between 18 and 55.');
-    return;
-  }
-
-  // Generate a unique user key using the current timestamp
-  const userKey = 'user_' + Date.now();
-
-  // Create a user object with the form data
-  const userData = {
-    name: form.name.value,
-    email: form.email.value,
-    password: form.password.value,
-    dob: form.dob.value,
-    acceptedTerms: form.acceptedTerms.checked,
-  };
-
-  // Store the user data in localStorage using the generated key
-  localStorage.setItem(userKey, JSON.stringify(userData));
-
-  // Add the user data to the table
-  addRowToTable(userTableBody, userData);
-});
-// Function to add a new user row to the table
-function addRowToTable(tableBody, userData) {
-  // Create a new table row
-  const newRow = tableBody.insertRow();
-
-  // Set a consistent style for all cells
-  const cellStyle = 'border border-gray-300 p-2';
-
-  // Add cells for each user data property
-  const nameCell = newRow.insertCell();
-  nameCell.textContent = userData.name;
-  nameCell.className = cellStyle;
-
-  const emailCell = newRow.insertCell();
-  emailCell.textContent = userData.email;
-  emailCell.className = cellStyle;
-
-  const passwordCell = newRow.insertCell();
-  passwordCell.textContent = userData.password;
-  passwordCell.className = cellStyle;
-
-  const dobCell = newRow.insertCell();
-  dobCell.textContent = userData.dob;
-  dobCell.className = cellStyle;
-
-  const acceptedTermsCell = newRow.insertCell();
-  acceptedTermsCell.textContent = userData.acceptedTerms;
-  acceptedTermsCell.className = cellStyle;
+const saveUserForm=(event)=>{
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTermsAndconditions = document.getElementById("acceptTerms").checked;
+    let year = Number(dob.split("-")[0]);
+    let today = new Date();
+    let y = today.getFullYear();
+    let age=y-year;
+    if(age>55 || age<18 ){
+        document.getElementById('dob')
+        return alert("Age should be between 18 and 55")
+    }
+    else{
+        const entry = {
+            name,
+            email,
+            password,
+            dob,
+            acceptedTermsAndconditions
+    };
+      userEntries=retrieveEntries();
+    userEntries.push(entry);
+    localStorage.setItem("user-entries", JSON.stringify(userEntries));
+    displayEntries();
+    userForm.reset();
 }
+};
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
+
+ 
+
 
       
 
